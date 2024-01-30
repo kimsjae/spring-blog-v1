6368a3485b1ad4816617e6f35a891bff73419cb7
kimseongjae <stwin755@gmail.com>
2024-01-30 15:09:17 +0900

    로그인 완료

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -1,8 +1,11 @@
 package shop.mtcoding.blog.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * 컨트롤러<p></p>
@@ -15,14 +18,33 @@ import org.springframework.web.bind.annotation.PostMapping;
  * 7. DB처리를 원하면 Model(DAO)에게 위임 후 View를 응답하면 끝
  */
 
+@RequiredArgsConstructor
 @Controller
 public class UserController {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final HttpSession session;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+
+    @PostMapping("/login")
+    public String login(UserRequest.LoginDTO requestDTO){
+        // 1. 유효성 검사
+        if(requestDTO.getUsername().length() < 3){
+            return "error/400";
+        }
+
+        // 2. 모델 필요 select * from user_tb where username=? and password=?
+        User user = userRepository.findByUsernameAndPassword(requestDTO);
+
+        if (user == null) {
+            return "error/401";
+        } else {
+            session.setAttribute("sessionUser", user);
+            // 3. 응답
+            return "redirect:/";
+        }
     }
+
 
     @PostMapping("/join")
     public String join(UserRequest.JoinDTO requestDTO) {
@@ -34,9 +56,9 @@ public class UserController {
         }
 
         // 2. Model에게 위임하기
-        userRepository.saveV2(requestDTO);
+        userRepository.save(requestDTO);
 
-
+        // 3. 응답
         return "redirect:/loginForm";
     }
 
@@ -59,4 +81,6 @@ public class UserController {
     public String logout() {
         return "redirect:/";
     }
+
+
 }
